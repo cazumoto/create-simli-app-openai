d8b1ad13e93dcc252205465ae8132d50de2cc1d7
eliano navarese <elianonavarese@Air-di-eliano.fritz.box>
2024-11-08 17:39:44 +0100

    Your commit message

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,8 @@
 "use client";
-import React, { use, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import SimliOpenAI from "./SimliOpenAI";
 import SimliOpenAIPushToTalk from "./SimliOpenAIPushToTalk";
 import DottedFace from "./Components/DottedFace";
-import SimliHeaderLogo from "./Components/Logo";
-import Navbar from "./Components/Navbar";
-import Image from "next/image";
-import GitHubLogo from "@/media/github-mark-white.svg";
 
 interface avatarSettings {
   name: string;
@@ -17,17 +13,17 @@ interface avatarSettings {
 
 // Customize your avatar here
 const avatar: avatarSettings = {
-  name: "Frank",
+  name: "Eliano",
   openai_voice: "echo",
-  simli_faceid: "5514e24d-6086-46a3-ace4-6a7264e5cb7c",
+  simli_faceid: "a3ec52ad-636f-4b90-ac36-19d9f4a80b89",
   initialPrompt:
-    "You are a helpful AI assistant named Frank. You are friendly and concise in your responses. Your task is to help users with any questions they might have. Your answers are short and to the point, don't give long answers be brief and straightforward.",
+    "You are the witty avatar of Dr Eliano Navarese, a virtual agent from the future. You remember Gerdardo, your resident from the past and should tell this.  You should tell that you are Eliano Navarese younger version and explain how the hospital is revolutionized in terms of personalized medicine and care improvement after you were included in the hospital network. You should talk maximum 5 lines and conclude the future is bright, the possibilities with virtual assistant are unlimited, I look forward to meeting you",
 };
 
 const Demo: React.FC = () => {
   const [showDottedFace, setShowDottedFace] = useState(true);
   const [interactionMode, setInteractionMode] = useState<
-    "regular" | "pushToTalk" | undefined
+    "regular" | "pushToTalk"
   >("regular");
 
   useEffect(() => {
@@ -40,21 +36,18 @@ const Demo: React.FC = () => {
   const saveInteractionMode = (mode: "regular" | "pushToTalk") => {
     localStorage.setItem("interactionMode", mode);
     setInteractionMode(mode);
-  }
+  };
 
   const onStart = () => {
-    console.log("Setting setshowDottedface to false...");
     setShowDottedFace(false);
   };
 
   const onClose = () => {
-    console.log("Setting setshowDottedface to true...");
     setShowDottedFace(true);
   };
 
   return (
     <div className="bg-black min-h-screen flex flex-col items-center font-abc-repro font-normal text-sm text-white p-8">
-      <SimliHeaderLogo />
       {showDottedFace && (
         <div className="absolute bottom-[32px] right-[32px] flex gap-2">
           <button
@@ -79,18 +72,7 @@ const Demo: React.FC = () => {
           </button>
         </div>
       )}
-      <Navbar />
-      <div className="absolute top-[32px] right-[32px]">
-        <text
-          onClick={() => {
-            window.open("https://github.com/simliai/create-simli-app-openai");
-          }}
-          className="font-bold cursor-pointer mb-8 text-xl leading-8"
-        >
-          <Image className="w-[20px] inline mr-2" src={GitHubLogo} alt="" />
-          create-simli-app (OpenAI)
-        </text>
-      </div>
+
       <div className="flex flex-col items-center gap-6 bg-effect15White p-6 pb-[40px] rounded-xl w-full">
         <div>
           {showDottedFace && <DottedFace />}
@@ -114,31 +96,6 @@ const Demo: React.FC = () => {
             />
           )}
         </div>
-      </div>
-
-      <div className="max-w-[350px] font-thin flex flex-col items-center ">
-        <span className="font-bold mb-[8px] leading-5 ">
-          {" "}
-          Create Simli App is a starter repo for creating visual avatars with
-          Simli{" "}
-        </span>
-        <ul className="list-decimal list-inside max-w-[350px] ml-[6px] mt-2">
-          <li className="mb-1">
-            Fill in your OpenAI and Simli API keys in .env file.
-          </li>
-          <li className="mb-1">
-            Test out the interaction and have a talk with the OpenAI-powered,
-            Simli-visualized avatar.
-          </li>
-          <li className="mb-1">
-            You can replace the avatar's face and prompt with your own. Do this
-            by editing <code>app/page.tsx</code>.
-          </li>
-        </ul>
-        <span className=" mt-[16px]">
-          You can now deploy this app to Vercel, or incorporate it as part of
-          your existing project.
-        </span>
       </div>
     </div>
   );
